746d45fe173587b75290669eec09370c7658db2c
Kovid Kunduru <kovid@Kovids-MacBook-Air.local>
2025-09-01 15:17:27 +1000

    Change in CategoryModel

--- src/models/Category/CategoryModel.js
+++ src/models/Category/CategoryModel.js
@@ -1,4 +1,5 @@
-import categoryCollection from "./categorySchema.js";
+// import categoryCollection from "./categorySchema.js";
+import categoryCollection from "./CategorySchema.js";
 
 export const getCategoryById = async (_id) => {
   return await categoryCollection.findById({ _id });
