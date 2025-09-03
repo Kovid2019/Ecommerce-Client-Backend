34d4f07daeeb894df535d29505d9eae8a42d0edb
Kovid Kunduru <kovid@Kovids-MacBook-Air.local>
2025-09-03 23:41:51 +1000

    Changes in cors of server.js

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use(
       "http://localhost:5173",
       "Ecommerce-Client-Backend-env.eba-ssm7eq3x.ap-southeast-2.elasticbeanstalk.com",
     ],
+    method: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
